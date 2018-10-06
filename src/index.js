9dc9c9f59f5bc8acd81a62db59249cf62997853d
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-10-05 21:34:52 -0700

    add bundle, created components for search bar, video items, video details and ensured search fuctionality worked with userinputs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDmDRaTqN7p3QpLYN70M-tkehPWdlGkleE';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboard');
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (data) => {
+      this.setState({videos: data,
+        selectedVideo: data[0]
+       });
+    });
+  }
+
+  render() {
+  return (
+  <div>
+    <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+    <VideoDetail video={this.state.selectedVideo}/>
+    <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+      videos={this.state.videos}/>
+  </div>
+  );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
